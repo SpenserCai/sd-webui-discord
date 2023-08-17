c5a09716a9817f1f60acc83b26222f0ca0648832
SpenserCai <spensercai@gmail.com>
2023-08-17 23:28:42 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
+###
+ # @Author: SpenserCai
+ # @Date: 2023-08-17 11:04:55
+ # @version: 
+ # @LastEditors: SpenserCai
+ # @LastEditTime: 2023-08-17 23:28:05
+ # @Description: file content
+### 
+export GOOS=linux
 go build -o "./release/sd-webui-discord"
+
+export GOOS=windows
+go build -o "./release/sd-webui-discord.exe"
 # 判断是否存在config.json
 if [ ! -f "./release/config.json" ]; then
     echo "config.json not found, copy config.example.json to config.json"
