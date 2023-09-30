310b00e475c3cb2eb4601e6cdc9264152ddff163
SpenserCai <spensercai@gmail.com>
2023-10-01 00:19:24 +0800

    jwt build,auth api support

--- api/business/auth.go
+++ api/business/auth.go
@@ -3,14 +3,18 @@
  * @Date: 2023-09-30 12:53:43
  * @version:
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-09-30 21:58:49
+ * @LastEditTime: 2023-10-01 00:13:56
  * @Description: file content
  */
 package business
 
 import (
+	"log"
+
 	ServiceOperations "github.com/SpenserCai/sd-webui-discord/api/gen/restapi/operations/user"
+	apiMiddleware "github.com/SpenserCai/sd-webui-discord/api/middleware"
 	"github.com/SpenserCai/sd-webui-discord/global"
+	DbotUser "github.com/SpenserCai/sd-webui-discord/user"
 	"github.com/bwmarrin/discordgo"
 	"github.com/go-openapi/runtime/middleware"
 )
@@ -43,8 +47,32 @@ func (b BusinessBase) SetAuthHandler() {
 		if err != nil {
 			return ServiceOperations.NewAuthFound().WithLocation("/error?error=auth_error")
 		}
+		rsgMsg, err := global.UserCenterSvc.RegisterUser(&DbotUser.UserInfo{
+			Id:   user.ID,
+			Name: user.Username,
+		})
+		if err != nil {
+			log.Println("RegisterUser error:", err)
+			return ServiceOperations.NewAuthFound().WithLocation("/error?error=login_error")
+		}
+		if rsgMsg != "REGISTERED USER INFO" && rsgMsg != "UPDATED USER INFO" {
+			return ServiceOperations.NewAuthFound().WithLocation("/error?error=login_error|" + rsgMsg)
+		}
+		userInfo, err := global.UserCenterSvc.GetUserInfo(user.ID)
+		if err != nil {
+			log.Println("GetUserInfo error:", err)
+			return ServiceOperations.NewAuthFound().WithLocation("/error?error=login_error")
+		}
+		// 通过用户信息构建jwt，包含用户id，用户名，角色列表，过期时间
+		jwt, err := apiMiddleware.BuildJwt(*userInfo, map[string]string{
+			"avatar": user.AvatarURL(""),
+		})
+		if err != nil {
+			log.Println("BuildJwt error:", err)
+			return ServiceOperations.NewAuthFound().WithLocation("/error?error=login_error")
+		}
 		// 获取用户信息
-		return ServiceOperations.NewAuthFound().WithLocation("/success?id=" + user.ID + "&username=" + user.Username)
+		return ServiceOperations.NewAuthFound().WithLocation("/login?token=" + jwt)
 	})
 
 }
--- api/middleware/validate_jwt.go
+++ api/middleware/validate_jwt.go
@@ -3,7 +3,7 @@
  * @Date: 2023-09-29 19:27:29
  * @version:
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-09-29 20:10:01
+ * @LastEditTime: 2023-09-30 23:03:13
  * @Description: file content
  */
 package middleware
@@ -12,8 +12,11 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"log"
+
+	DbotUser "github.com/SpenserCai/sd-webui-discord/user"
 
 	"github.com/SpenserCai/sd-webui-discord/global"
 	"github.com/golang-jwt/jwt/v5"
@@ -26,14 +29,57 @@ func ValidateJwt(bearerHeader string) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("error decoding token")
 		}
-		return global.Config.WebSite.Api.JwtSecret, nil
+		return []byte(global.Config.WebSite.Api.JwtSecret), nil
 	})
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
 	if token.Valid {
+		// 验证token是否过期
+		if claims["exp"].(float64) < float64(time.Now().Unix()) {
+			return nil, errors.New("token expired")
+		}
 		return claims["id"].(string), nil
 	}
 	return nil, errors.New("invalid token")
+}
+
+func BuildJwt(userInfo DbotUser.UserInfo, other map[string]string) (string, error) {
+	claims := jwt.MapClaims{
+		"id":   userInfo.Id,
+		"name": userInfo.Name,
+		"role": userInfo.Roles,
+	}
+	for k, v := range other {
+		claims[k] = v
+	}
+	// 超时时间7天
+	claims["exp"] = time.Now().Add(time.Hour * 24 * 7).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(global.Config.WebSite.Api.JwtSecret))
+}
+
+// 刷新jwt
+func RefreshJwt(bearerHeader string) (string, error) {
+	bearerToken := strings.Split(bearerHeader, " ")[1]
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(bearerToken, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			log.Println("error decoding token")
+			return nil, fmt.Errorf("error decoding token")
+		}
+		return []byte(global.Config.WebSite.Api.JwtSecret), nil
+	})
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	if token.Valid {
+		// 超时时间7天
+		claims["exp"] = time.Now().Add(time.Hour * 24 * 7).Unix()
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		return token.SignedString(global.Config.WebSite.Api.JwtSecret)
+	}
+	return "", errors.New("invalid token")
 }
