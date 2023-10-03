6274e7034733c3c0759abdcdd7710ef294fcce0b
SpenserCai <spensercai@gmail.com>
2023-10-04 02:04:48 +0800

    update website home

---
+++ website/src/components/SectionTitleLine.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import BaseIcon from '@/components/BaseIcon.vue'
+import IconRounded from '@/components/IconRounded.vue'
+
+defineProps({
+  icon: {
+    type: String,
+    default: null
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  main: Boolean
+})
+
+</script>
+
+<template>
+  <section :class="{ 'pt-6': !main }" class="mb-6 flex items-center justify-between">
+    <div class="flex items-center justify-start">
+      <IconRounded v-if="icon && main" :icon="icon" color="light" class="mr-3" bg />
+      <BaseIcon v-else-if="icon" :path="icon" class="mr-2" size="20" />
+      <h1 :class="main ? 'text-3xl' : 'text-2xl'" class="leading-tight">
+        {{ title }}
+      </h1>
+    </div>
+  </section>
+</template>
--- website/src/views/AppView.vue
+++ website/src/views/AppView.vue
@@ -3,7 +3,7 @@
  * @Date: 2023-10-03 16:06:09
  * @version: 
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-10-04 01:09:55
+ * @LastEditTime: 2023-10-04 01:51:12
  * @Description: file content
 -->
 <script setup>
@@ -11,8 +11,10 @@ import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 import UserCard from '@/components/UserCard.vue'
 import SectionMain from '@/components/SectionMain.vue'
 import CardBox from '@/components/CardBox.vue'
+import SectionTitleLine from '@/components/SectionTitleLine.vue'
 import { useMainStore } from '@/stores/main'
-import { Input } from 'flowbite-vue'
+import { Input,Textarea } from 'flowbite-vue'
+import { mdiApplicationSettings } from '@mdi/js'
 
 const mainStore = useMainStore()
 </script>
@@ -21,41 +23,52 @@ const mainStore = useMainStore()
   <LayoutAuthenticated>
     <SectionMain>
       <UserCard class="mb-6" />
-        <CardBox is-form>
-          <div class="grid grid-cols-2 gap-2">
-            <Input
-              v-model="mainStore.stableConfig.sd_model_checkpoint"
-              label="Checkpoints"
-              disabled>
-            </Input>
-            <Input
-              v-model="mainStore.stableConfig.sd_vae"
-              label="Vae"
-              disabled>
-            </Input>
-            <Input
-              v-model="mainStore.stableConfig.height"
-              label="Height"
-              disabled>
-            </Input>
-            <Input
-              v-model="mainStore.stableConfig.width"
-              label="Width"
-              disabled>
-            </Input>
-            <Input
-              v-model="mainStore.stableConfig.steps"
-              label="Steps"
-              disabled>
-            </Input>
-            <Input
-              v-model="mainStore.stableConfig.cfg_scale"
-              label="Cfg Scale"
-              disabled>
-            </Input>
-          </div>
-
-        </CardBox>
+      <SectionTitleLine :main="false" title="Setting" :icon="mdiApplicationSettings" />
+      <CardBox is-form>
+        <div class="grid grid-cols-2 gap-2">
+          <Input
+            v-model="mainStore.stableConfig.sd_model_checkpoint"
+            label="Checkpoints"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.sd_vae"
+            label="Vae"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.sampler"
+            label="Sampler"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.CLIP_stop_at_last_layers"
+            label="CLIP Skip"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.height"
+            label="Height"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.width"
+            label="Width"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.steps"
+            label="Steps"
+            disabled>
+          </Input>
+          <Input
+            v-model="mainStore.stableConfig.cfg_scale"
+            label="Cfg Scale"
+            disabled>
+          </Input>
+          <Textarea v-model="mainStore.stableConfig.negative_prompt" class="col-span-2" rows="4" label="Negative Prompt" />
+        </div>
+      </CardBox>
 
     </SectionMain>
   </LayoutAuthenticated>
