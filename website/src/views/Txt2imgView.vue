b97a12f5327d2e41e558057f010a5916ed105ce7
SpenserCai <spensercai@gmail.com>
2023-10-06 18:39:53 +0800

    delete not use code

--- website/src/views/Txt2imgView.vue
+++ website/src/views/Txt2imgView.vue
@@ -3,7 +3,7 @@
  * @Date: 2023-10-06 17:25:44
  * @version: 
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-10-06 18:33:10
+ * @LastEditTime: 2023-10-06 18:39:42
  * @Description: file content
 -->
 <script setup>
@@ -53,50 +53,6 @@ getListFunc(1, 12)
                 <img class="h-auto max-w-full rounded-lg" :src="currentList[index*3+i_index].images[0]" alt="">
             </div>
           </div>
-          <!--<div class="grid gap-4">
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
-              </div>
-          </div>
-          <div class="grid gap-4">
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
-              </div>
-          </div>
-          <div class="grid gap-4">
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
-              </div>
-          </div>
-          <div class="grid gap-4">
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
-              </div>
-              <div>
-                  <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
-              </div>
-          </div>-->
         </div>
 
     </SectionMain>
